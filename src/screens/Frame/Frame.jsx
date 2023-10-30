2dabc09f34cf5801cefb728193102d1f612d5e9c
CodeSandbox <support@codesandbox.io>
2023-10-30 23:24:31 +0000

    Initial commit

---
+++ src/screens/Frame/Frame.jsx
@@ -0,0 +1,111 @@
+import React from "react";
+import "./style.css";
+
+export const Frame = () => {
+  return (
+    <div className="frame">
+      <div className="overlap-wrapper">
+        <div className="overlap">
+          <div className="kitchen-tiles">
+            <div className="overlap-group">
+              <div className="div">
+                <header className="header">
+                  <img className="element" alt="Element" src="https://c.animaapp.com/RyJnyUrG/img/29015-2@2x.png" />
+                  <img className="img" alt="Element" src="https://c.animaapp.com/RyJnyUrG/img/29015-2@2x.png" />
+                  <img className="element-2" alt="Element" src="https://c.animaapp.com/RyJnyUrG/img/29015-2@2x.png" />
+                  <img className="element-3" alt="Element" src="https://c.animaapp.com/RyJnyUrG/img/29015-2@2x.png" />
+                  <img className="element-4" alt="Element" src="https://c.animaapp.com/RyJnyUrG/img/29015-5@2x.png" />
+                  <img className="element-5" alt="Element" src="https://c.animaapp.com/RyJnyUrG/img/29015-10@2x.png" />
+                </header>
+                <img
+                  className="marco-izquierdo"
+                  alt="Marco izquierdo"
+                  src="https://c.animaapp.com/RyJnyUrG/img/marco-izquierdo.png"
+                />
+                <div className="header-de-abajo">
+                  <img className="element-6" alt="Element" src="https://c.animaapp.com/RyJnyUrG/img/29015-23@2x.png" />
+                  <img className="element-7" alt="Element" src="https://c.animaapp.com/RyJnyUrG/img/29015-33@2x.png" />
+                  <img className="element-8" alt="Element" src="https://c.animaapp.com/RyJnyUrG/img/29015-33@2x.png" />
+                  <img className="element-9" alt="Element" src="https://c.animaapp.com/RyJnyUrG/img/29015-33@2x.png" />
+                  <img className="element-10" alt="Element" src="https://c.animaapp.com/RyJnyUrG/img/29015-33@2x.png" />
+                  <img className="element-11" alt="Element" src="https://c.animaapp.com/RyJnyUrG/img/29015-37@2x.png" />
+                </div>
+              </div>
+              <div className="marco-derecho">
+                <div className="overlap-group-2">
+                  <img className="element-12" alt="Element" src="https://c.animaapp.com/RyJnyUrG/img/29015-10@2x.png" />
+                  <img className="element-13" alt="Element" src="https://c.animaapp.com/RyJnyUrG/img/29015-10@2x.png" />
+                </div>
+                <img className="element-14" alt="Element" src="https://c.animaapp.com/RyJnyUrG/img/29015-10@2x.png" />
+                <img className="element-15" alt="Element" src="https://c.animaapp.com/RyJnyUrG/img/29015-10@2x.png" />
+                <img className="element-16" alt="Element" src="https://c.animaapp.com/RyJnyUrG/img/29015-10@2x.png" />
+                <img className="element-17" alt="Element" src="https://c.animaapp.com/RyJnyUrG/img/29015-10@2x.png" />
+                <img className="element-18" alt="Element" src="https://c.animaapp.com/RyJnyUrG/img/29015-10@2x.png" />
+                <img className="element-19" alt="Element" src="https://c.animaapp.com/RyJnyUrG/img/29015-10@2x.png" />
+              </div>
+            </div>
+          </div>
+          <div className="uno-no-roba-su">
+            <p className="personaliza-y-cuida">
+              Personaliza y cuida tu propia despensa.
+              <br />
+              Construye tu propia identidad
+            </p>
+          </div>
+          <div className="mensaje-de-pagos">
+            <p className="si-un-producto-no-se">
+              Si un producto no se pagó, asumiremos que se te olvidó.
+              <br />
+              <br />
+              Si toda una compra no se pagó, asumiremos que te quedaste sin plata y no te diste cuenta.
+            </p>
+            <img className="buena-fe" alt="Buena fe" src="https://c.animaapp.com/RyJnyUrG/img/buena-fe--1@2x.png" />
+          </div>
+          <div className="escanea-y-paga">
+            <p className="text-wrapper">El usuario asume el control del proceso</p>
+            <img
+              className="screenshot"
+              alt="Screenshot"
+              src="https://c.animaapp.com/RyJnyUrG/img/screenshot-2023-10-15-at-11-26-2@2x.png"
+            />
+          </div>
+          <div className="seguridad">
+            <div className="text-wrapper-2">Seguridad: Visión por computador</div>
+            <img className="CV" alt="Cv" src="https://c.animaapp.com/RyJnyUrG/img/cv-1@2x.png" />
+          </div>
+          <div className="animacin-de-los">
+            <div className="overlap-2">
+              <p className="p">Animación de los usuarios usando el producto</p>
+              <img
+                className="imagen-demo"
+                alt="Imagen demo"
+                src="https://c.animaapp.com/RyJnyUrG/img/imagen-demo-servimarket-1.png"
+              />
+            </div>
+          </div>
+          <div className="presentacin-del">
+            <p className="text-wrapper-3">
+              Nuestras vitrinas son abiertas porque confiamos en nuestra comunidad colombiana
+            </p>
+          </div>
+          <div className="div-2">
+            <div className="porque" />
+            <div className="porque-2" />
+            <div className="porque-3" />
+          </div>
+          <p className="imagen-deslizante">
+            <span className="span">Imagen deslizante que explica </span>
+            <span className="text-wrapper-4">el porque</span>
+            <span className="span"> estamos haciendo esto</span>
+          </p>
+          <img className="misin" alt="Misin" src="https://c.animaapp.com/RyJnyUrG/img/misi-n.png" />
+          <img
+            className="barra-de-opciones"
+            alt="Barra de opciones"
+            src="https://c.animaapp.com/RyJnyUrG/img/barra-de-opciones.png"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
